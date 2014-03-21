6e8688d56b124cb0e4f11fea060cdd1208363f5f
Adam Balachowski <adam@designer-ab.de>
2014-03-21 12:32:13 +0100

    Updated version number

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ install_requires = [
 
 setup(
     name='sentry-redmine',
-    version='0.1.0',
+    version='0.2.0',
     author='Aaditya Sood, Adam Balachowski',
     author_email='a@ideadevice.com',
     url='https://github.com/Adam16/sentry-redmine',
